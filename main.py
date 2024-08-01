6a17c0bb36e6d2c1b2aee663677284aa688f2c95
burnsyhimself <brettburnsj@gmail.com>
2024-08-01 17:42:10 -0500

    Step 10 Completed

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 def main():
     book_path = "/Users/brettburns/workspace/github.com/0xburnsy/bookbot/books/frankenstein.txt"
-    text = get_book_text(book_path)
-    print(get_word_count(text))
+    text = get_book_text(book_path).lower()
+    words = get_words(text)   
+    num_words = len(words)
+    print(f"This book contains {num_words} words.")
+    print(get_character_count(words))
 
 def get_book_text(book_path):
     with open(book_path) as f:
         return f.read()
 
-def get_word_count(text):
-    return len(text.split())
+def get_words(text):
+    return text.split()
+
+def get_character_count(words):
+    character_dictionary = {}
+    for word in words:
+        for character in word:
+            if character in character_dictionary:
+                character_dictionary[character] += 1
+            else:
+                character_dictionary[character] = 1
+    return character_dictionary
 
 if __name__ == "__main__":
     main()
